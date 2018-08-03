97949eb2521b6aede1b5fc9f939e8c0d6f70e5d0
Artur RSO <artur.rso@outlook.com>
2018-08-03 20:38:05 -0300

    Configurando projeto
    Adicionando arquivos de configuração da IDE e do Maven

---
+++ api-users/src/main/java/artur/rso/api/users/ApiUsersApplication.java
@@ -0,0 +1,12 @@
+package artur.rso.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsersApplication.class, args);
+	}
+}
