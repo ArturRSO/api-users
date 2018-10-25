a447f347cb746fc8cb75d7f02770d5cd3cc90f5c
Artur RSO <artur.oliveira@cedrotech.com>
2018-10-25 15:54:51 -0300

    Atualizações
    - Atualizando README
    - Ajustando codificação de texto dos arquivos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # api-users
-API para cadastro, busca e exclusão de usuários.
+- API para cadastro, busca e exclusão de usuários.
+- Também possui um sistema de autenticação.
+- Projeto feito com Spring.
--- src/main/java/artur/rso/api/users/enums/HttpResponseEnum.java
+++ src/main/java/artur/rso/api/users/enums/HttpResponseEnum.java
@@ -2,16 +2,16 @@ package artur.rso.api.users.enums;
 
 public enum HttpResponseEnum {
 	
-	GENERIC_ERROR(1, "Um erro ocorreu e a operação não pôde ser concluída.", "500 Internal Server Error"),
-	GENERIC_NOT_FOUND(2, "A busca não encontrou nenhum registro.", "200 OK"),	
+	GENERIC_ERROR(1, "Um erro ocorreu e a operaÃ§Ã£o nÃ£o pÃ´de ser concluÃ­da.", "500 Internal Server Error"),
+	GENERIC_NOT_FOUND(2, "A busca nÃ£o encontrou nenhum registro.", "200 OK"),	
 	CREATION_SUCCESS(3, "Cadastrado com sucesso!", "200 OK"),
 	UPDATE_SUCCESS(4, " Atualizado com sucesso!", "200 OK"),
-	DELETION_SUCCESS(5, "Registro excluído com sucesso!", "200 OK"),
-	DELETION_SUCCESS_PLURAL(6, "Registros excluídos com sucesso", "200 OK"),
-	ALREADY_DELETED(7, "Este registro já se encontra inativo.", "200 OK"),
-	ALREADY_DELETED_PLURAL(8, "Estes registros já se encontram inativos", "200 OK"),
-	LOGOUT(9, "Você saiu com sucesso.", "200 OK"),
-	LOGIN(10, "Você entrou no sistema com sucesso!", "200 OK");
+	DELETION_SUCCESS(5, "Registro excluÃ­do com sucesso!", "200 OK"),
+	DELETION_SUCCESS_PLURAL(6, "Registros excluÃ­dos com sucesso", "200 OK"),
+	ALREADY_DELETED(7, "Este registro jÃ¡ se encontra inativo.", "200 OK"),
+	ALREADY_DELETED_PLURAL(8, "Estes registros jÃ¡ se encontram inativos", "200 OK"),
+	LOGOUT(9, "VocÃª saiu do sistema com sucesso.", "200 OK"),
+	LOGIN(10, "VocÃª entrou no sistema com sucesso!", "200 OK");
 	
 	private final int id;
 	private final String message;
